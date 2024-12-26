1f4e14c37e00dd4207e23c1be8be5c98b5d6955e
Stef <151558411+Berryfeels@users.noreply.github.com>
2024-12-26 18:43:22 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,7 +28,14 @@ int	main()
 {	
 	int	fd;
 	bld_in	*builtins;
+	int fd_error;
 
+    fd_error = open("Error_log.txt", O_RDWR | O_CREAT | O_APPEND, 0777);
+    if (fd_error < 0)
+    {
+        perror("Failed to open error log file");
+        return 1;
+    }
 	while ((fd = open("console", O_RDWR)) >= 0)
 	{
 		if (fd >= 3)
@@ -37,11 +44,11 @@ int	main()
 			break;
 		}
 	}
+	ft_dprintf(fd_error, "-----ERROR LOG-----\n");
 	builtins = create_builtin_list();
 	shell_loop (builtins);
-
 	free_builtin_list(builtins);
-
-	close (fd);
+	close(fd);
+	close(fd_error);
 	return 0;
 }
